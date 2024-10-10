4d1640e3958e4b1f1cd625e62c1ade786ce4df8b
Satolh <santiitoledo7@gmail.com>
2024-10-10 10:42:17 -0300

    segundo commit en le footer

--- src/componentes/FooterRiver.jsx
+++ src/componentes/FooterRiver.jsx
@@ -13,8 +13,8 @@ const FooterRiver = () => {
         </div>
 
         <div className='container-redes'>
-          <a target='_blank' className='a-icon-redes' href='#'><FaInstagram className='icon-redes' /></a>
-          <a target='_blank' className='a-icon-redes' href='#'><FaTwitter className='icon-redes' /></a>
+          <a  className='a-icon-redes' href='#'><FaInstagram className='icon-redes' /></a>
+          <a  className='a-icon-redes' href='#'><FaTwitter className='icon-redes' /></a>
           <a target='_blank' className='a-icon-redes' href='https://github.com/satolh'><FaGithub className='icon-redes'  /></a>
         </div>
 
